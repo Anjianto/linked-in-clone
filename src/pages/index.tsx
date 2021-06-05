e17c6ba0b3d01ce9e17e481e0cc7e56d1e8546d1
Anjianto <anjianto06@gmail.com>
2021-06-05 19:24:45 +0700

    Setup Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav";
+import Main from "../parts/Home/Main";
+
+export default function index() {
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  );
+}
